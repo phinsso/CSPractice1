013a9870237124eb12bcc38c0f8d97d4cb014896
phinsso <naso01247@gmail.com>
2024-06-27 12:16:23 +0900

    #3 14-1. 반복문과 배열

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -125,6 +125,10 @@ namespace CSPractice1
 
             // #2 13-6. 삼항 연산자
             Console.WriteLine(number % 2 == 0 ? true : false);
+
+            // #3 14-1. 반복문과 배열
+            int[] intArray = new int[100];
+            Console.WriteLine(intArray[99]);
         }
     }
 }
