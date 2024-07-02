452b3112849d537e8d4e201c49fe91e73f4dd754
phinsso <naso01247@gmail.com>
2024-07-02 15:09:17 +0900

    #6 23-6. 상속의 생성자

---
+++ CSPractice1/Parent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSPractice1
+{
+    internal class Parent
+    {
+        public Parent()
+        {
+            Console.WriteLine("부모 생성자 호출");
+        }
+
+        public Parent(string input)
+        {
+            Console.WriteLine(input);
+        }
+    }
+}
--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -319,6 +320,10 @@ namespace CSPractice1
                     if(dog != null) { dog.Bark(); }
                 }
             }
+
+            // #6 23-6. 상속의 생성자
+            Child child = new Child();
+            Child child2 = new Child("홍길동");
             
             }
         }
